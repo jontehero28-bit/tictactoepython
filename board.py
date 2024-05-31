cf54dcdf613b3345de6a464324d10c2d8f61538e
Yuriy Petrov <Jurijp2005@gmail.com>
2024-05-31 11:47:43 +0200

    commit?

--- board.py
+++ board.py
@@ -5,16 +5,3 @@ import numpy as np #used to work with arrays and do some mathematic equations
 from main import *
 from constant import *
 
-class Board:
-    
-    def __init__(self):
-        self.squares = np.zeros( (ROWS, COLS)) #based on the amount of rows and cols prints a board with zeroes
-        self.mark_square(1, 1, 2)
-        print(self.squares)
-        
-    def mark_square(self, row, col, player):
-        #program works by follwing setup:
-        #when player(1 or 2) clicks on a square it becomes marked with 1 or 2
-        #one is for cross, and 2 for circles
-        
-        self.squares[row][col] = player #square number is player number.
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import numpy as np
 
 #import classes
 from constant import *
-from board import Board
 
 #create screen setup
 p.init()
@@ -13,10 +12,28 @@ screen = p.display.set_mode( (WIDTH, HEIGHT) )
 p.display.set_caption("TicTacToe")
 screen.fill( BKG_COLOR ) 
 
+class Board:
+    
+    def __init__(self):
+        self.squares = np.zeros( (ROWS, COLS)) #based on the amount of rows and cols prints a board with zeroes
+        
+        
+    def mark_square(self, row, col, player):
+        #program works by follwing setup:
+        #when player(1 or 2) clicks on a square it becomes marked with 1 or 2
+        #one is for cross, and 2 for circles
+        
+        self.squares[row][col] = player #square number is player number.
+        
+    def empty_square(self, row, col):
+        
+        return self.squares[row][col] == 0
+
 class Game:
     
     def __init__(self):
-        self.board = Board()  #from board class
+        self.board = Board()     #from board class
+        self.player = 1          #player1 turn in the beginning
         self.show_lines()
     
     def show_lines(self):
@@ -30,14 +47,30 @@ class Game:
 
 #mainLoop for the game
 def main_loop():
+    
+    #object for game
+    game = Game()
+    board = game.board  #so i would not write game.board all the time
+    
+    #mainloop
     while True:
-        game = Game()
 
         for event in p.event.get():
             
             if event.type == p.QUIT:
                 p.quit()
                 sys.exit()
+                
+            if event.type == p.MOUSEBUTTONDOWN:  #listens for mouseclickevent
+                pos = event.pos     #change value for pygame.pos so it would positions for my game
+                row = pos[1]  // SQUARE_SIZE
+                col = pos[0]  // SQUARE_SIZE
+                
+                if board.empty_square(row, col):
+                    board.mark_square(row, col, 1) #one is for player.
+                    print(board.squares)
+                
+            
                 
         p.display.update()  #can also use flip
 
